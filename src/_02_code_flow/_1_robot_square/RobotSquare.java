7dbfc0970dfdc3925980adec396b8dc398886ffe
tchengsd <tchengsd@gmail.com>
2022-05-14 16:30:51 -0700

    I learned how to make a square with a robot. I have to make all sides equal.

--- src/_02_code_flow/_1_robot_square/RobotSquare.java
+++ src/_02_code_flow/_1_robot_square/RobotSquare.java
@@ -17,30 +17,34 @@ public class RobotSquare {
 
         // PEN.  Put the robot's pen down so it can draw, Use this command:
 		 /**     rob.penDown();    **/
-
+    	//PEN.   Put the robot's pen down so it can draw, Use this command:
+    	         rob.penDown();
         // SPEED. Make the robot move quickly. Use this command:
 		  /**     rob.setSpeed();    **/
- 
+        rob.setSpeed(0);
     	// COUNT. Create an int variable that will count how many sides of the square we have drawn.
     	//        The start value will be zero because no sides have yet been drawn. Use this code:
 		  /**     int count = 0;    **/
-    	
+    	int count = 0;
+    	rob.move(200);
 
         // DRAW.  Draw a line. Use this command:
     		  /**     rob.move();    **/
   
 
 	// TURN.  Turn 90 degrees. Use this command:
-    		  /**     rob.turn();    **/
-
+    		  /**     rob.turn(); 
+    		      **/
+                  rob.turn(90);
     	
     	// INCREASE COUNT. Add one to the number of sides that have now been drawn like this:
     		  /**     count += 1;    **/
-
+                  count +=1;    
     	
     	// Run the program. Did the Robot draw a line and turn?
-    	 
-    	
+    	 rob.move(200);
+    	 rob.turn(90);
+    	count+=1;
         // LOOP.  Make a while loop and move the DRAW, TURN, and INCREASE COUNT code 
     	//        so it is between the curly braces like the example below. 
     	//        The code in the loop will now repeat 4 times to draw a square.
@@ -51,7 +55,13 @@ public class RobotSquare {
     	 *            INCREASE COUNT
     	 *      }
     	 **/
-    	
+    	while(count < 4) {
+    		rob.move(200);
+    		rob.turn(90);
+    		count+=1;
+    	}
+    			
+  
     	
     	// Run the program again. Did your Robot draw a square?
 
